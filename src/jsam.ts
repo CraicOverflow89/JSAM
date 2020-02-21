2babf75bda635eb781c1d2c9c358e6f0f06062c3
CraicOverflow89 <craic.overflow@hotmail.com>
2020-02-21 09:12:31 +0000

    Added global flag to fix some bugs. Fixed line break.

--- src/jsam.ts
+++ src/jsam.ts
@@ -2,34 +2,34 @@ class JSAM {
 	ruleList: Array<JSAMRule> = [
 
 		// Code Examples
-		this.rule(/```\n([^`]*)\n```/, "<pre><code>$1</code></pre>"),
-		this.rule(/`([^`]*)`/, "<pre><code>$1</code></pre>"),
+		this.rule(/```\n([^`]*)\n```/g, "<pre><code>$1</code></pre>"),
+		this.rule(/`([^`]*)`/g, "<pre><code>$1</code></pre>"),
 
 		// Multiline Headers
-		this.rule(/([^\n]*)\n(=)+/, "<h1>$1</h1>"),
-		this.rule(/([^\n]*)\n(-)+/, "<h2>$1</h2>"),
+		this.rule(/([^\n]*)\n(=)+\n/g, "<h1>$1</h1>"),
+		this.rule(/([^\n]*)\n(-)+\n/g, "<h2>$1</h2>"),
 
 		// Inline Headers
-		this.rule(/###### ([^#\n]*)\n/, "<h6>$1</h6>"),
-		this.rule(/##### ([^#\n]*)\n/, "<h5>$1</h5>"),
-		this.rule(/#### ([^#\n]*)\n/, "<h4>$1</h4>"),
-		this.rule(/### ([^#\n]*)\n/, "<h3>$1</h3>"),
-		this.rule(/## ([^#\n]*)\n/, "<h2>$1</h2>"),
-		this.rule(/# ([^#\n]*)\n/, "<h1>$1</h1>"),
+		this.rule(/###### ([^#\n]*)\n/g, "<h6>$1</h6>"),
+		this.rule(/##### ([^#\n]*)\n/g, "<h5>$1</h5>"),
+		this.rule(/#### ([^#\n]*)\n/g, "<h4>$1</h4>"),
+		this.rule(/### ([^#\n]*)\n/g, "<h3>$1</h3>"),
+		this.rule(/## ([^#\n]*)\n/g, "<h2>$1</h2>"),
+		this.rule(/# ([^#\n]*)\n/g, "<h1>$1</h1>"),
 
 		// Bold Format
-		this.rule(/\*\*([^*]*)\*\*/, "<b>$1</b>"),
-		this.rule(/__([^_]*)__/, "<b>$1</b>"),
+		this.rule(/\*\*([^*]*)\*\*/g, "<b>$1</b>"),
+		this.rule(/__([^_]*)__/g, "<b>$1</b>"),
 
 		// Italic Format
-		this.rule(/\*([^*]*)\*/, "<i>$1</i>"),
-		this.rule(/_([^_]*)_/, "<i>$1</i>"),
+		this.rule(/\*([^*]*)\*/g, "<i>$1</i>"),
+		this.rule(/_([^_]*)_/g, "<i>$1</i>"),
 
 		// Strikethrough Format
-		this.rule(/~~([^~]*)~~/, "<s>$1</s>"),
+		this.rule(/~~([^~]*)~~/g, "<s>$1</s>"),
 
 		// Line Break
-		this.rule(/\n\n/, "<br>")
+		this.rule(/\n\n/g, "<br><br>")
 	]
 
 	convert(element: HTMLElement) {
